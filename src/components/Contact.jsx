27b464f2a5024604610d1d7b846329673e19f934
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-09-18 03:04:26 +0530

    contact card added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,27 +1,30 @@
-import { Box, Flex, Heading, HStack, Icon, IconButton, VStack } from "@chakra-ui/react";
+import { Box, Flex, Heading, HStack, Icon, IconButton, Link, VStack } from "@chakra-ui/react";
 import React from "react";
 import { MdMarkEmailUnread } from "react-icons/md";
-import { Link } from "react-scroll";
+import { BsFillTelephoneInboundFill, BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
 
 const Contact = () => {
   return (
-    <VStack id="contact">
+    <VStack id="contact" gap="10">
       <Heading>Let's get in touch</Heading>
       <Flex>
-        <Box>
-          <Link>
-            <IconButton
-              colorScheme="teal"
-              aria-label="Call Segun"
-              size="lg"
-              icon={<MdMarkEmailUnread />}
-            />
+        <HStack gap="8" my="4">
+          <Link href="mailto:sanjay.g.258@gmail.com">
+            <Icon as={MdMarkEmailUnread} w={12} h={12} color="telegram.800" />
           </Link>
-
-          <Heading>Tel : Sanjay.g.258@gmail.com</Heading>
-          <Heading>Email : Sanjay.g.258@gmail.com</Heading>
-          <Heading>Email : Sanjay.g.258@gmail.com</Heading>
-        </Box>
+          <Link href="tel:+918237202188">
+            <Icon as={BsFillTelephoneInboundFill} w={12} h={12} color="telegram.800" />
+          </Link>
+          <Link href="https://twitter.com/IamSanjayGupta" target="_blank">
+            <Icon as={BsTwitter} w={12} h={12} color="telegram.800" />
+          </Link>
+          <Link href="https://www.linkedin.com/in/IamSanjayGupta" target="_blank">
+            <Icon as={BsLinkedin} w={12} h={12} color="telegram.800" />
+          </Link>
+          <Link href="https://github.com/IamSanjayGupta" target="_blank">
+            <Icon as={BsGithub} w={12} h={12} color="telegram.800" />
+          </Link>
+        </HStack>
         <Box></Box>
       </Flex>
     </VStack>
