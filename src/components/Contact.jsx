b4a6fdb82d19146da8cc96519a52e2ba5a9373d7
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-09-18 01:51:09 +0530

    deploy

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,22 @@
-import { Heading, VStack } from "@chakra-ui/react";
+import { Box, Flex, Heading, HStack, Icon, VStack } from "@chakra-ui/react";
 import React from "react";
+import { MdMarkEmailUnread } from "react-icons/md";
 
 const Contact = () => {
   return (
     <VStack id="contact">
-      <Heading>Contact</Heading>
+      <Heading>Let's get in touch</Heading>
+      <Flex>
+        <Box>
+          <Heading>
+            <Icon as={MdMarkEmailUnread} /> Email : Sanjay.g.258@gmail.com
+          </Heading>
+          <Heading>Tel : Sanjay.g.258@gmail.com</Heading>
+          <Heading>Email : Sanjay.g.258@gmail.com</Heading>
+          <Heading>Email : Sanjay.g.258@gmail.com</Heading>
+        </Box>
+        <Box></Box>
+      </Flex>
     </VStack>
   );
 };
